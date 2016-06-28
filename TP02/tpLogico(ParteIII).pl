1bb6989b3f901f5e7420a0b162387a20666fc297
fedevrs <fedeochea_95@hotmail.com>
2016-06-28 13:19:38 -0300

    ej 2 con respuestas duplicadas y ej 3

--- TP02/tpLogico(ParteIII).pl
+++ TP02/tpLogico(ParteIII).pl
@@ -18,3 +18,30 @@ subconjunto(L,[_|R1]):-subconjunto(L,R1).
 respetoTotal([],0).
 respetoTotal([Elem],Total):-nivelRespeto(Elem, Total).
 respetoTotal([Cab|Cola],Total):-nivelRespeto(Cab,Resp), respetoTotal(Cola,TotalCola), Total is (TotalCola + Resp).
+
+
+%Ejercicio 2: Batallon
+
+batallon(SubLista, Encargos):- 
+	lista(ListaCompleta),
+	subconjunto(SubLista,ListaCompleta), 
+	encargoTotal(SubLista,Total), 
+	Total > Encargos.
+
+lista(Lista):- findall(Nombre, (personaje(Nombre, _ ),respetable(Nombre)), Lista).
+
+subconjunto([],[]). 
+subconjunto([C|R],[C|R1]):-subconjunto(R,R1). 
+subconjunto(L,[_|R1]):-subconjunto(L,R1). 
+
+encargoTotal([],0).
+encargoTotal([Elem],Total):-cantidadEncargos(Elem, Total).
+encargoTotal([Cab|Cola],Total):-cantidadEncargos(Cab,Resp), encargoTotal(Cola,TotalCola), Total is (TotalCola + Resp).
+
+%Ejercicio 3: Quienes superan
+
+quienesSuperan(Lista, Elem, cuantosAmigos, ListaFinal):- findall(A, (member(A,Lista), cuantosAmigos(A,X), X > Elem), ListaFinal).
+quienesSuperan(Lista, Elem, cantidadEncargos, ListaFinal):- findall(A, (member(A,Lista), cantidadEncargos(A,X), X > Elem), ListaFinal).
+
+member(Elem,[Elem|_]).
+member(Elem, [_|Cola]):- member(Elem, Cola).
